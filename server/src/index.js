7c36a0e75d3765a32f745d9917a8a24b1ad54388
Pietra Soares <pietra.soares2013@hotmail.com>
2025-10-20 23:33:55 -0300

    Ambiente configurado e com teste de conexão com o BD.

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import connectDB from './config/db.js'; // importando o BD
+import Teste from './models/Teste.js'; // importando o teste do BD
+
+
+connectDB(); // BD
 
 const app = express();
 const PORT = 5000;
@@ -7,10 +12,34 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+// ROTA DO TESTE DO BD
+
+app.get('/api/teste-db', async (req, res) => {
+    try {
+        const novoTeste = new Teste();
+        await novoTeste.save();
+        const todosTestes = await Teste.find({});
+
+        return res.status(200).json({
+            status: 'SUCESSO TOTAL',
+            mensagem: 'Conexão Node -> MongoDB está 100% funcional. Inserção e Leitura OK.',
+            total_registros: todosTestes.length,
+            ultimo_registro_salvo: novoTeste
+        });
+    } catch (error) {
+        console.error('Erro na rota de teste de DB:', error);
+        return res.status(500).json({
+            status: 'ERRO NO DB',
+            mensagem: 'Falha na operação de banco de dados. Verifique o servidor mongod e a URI.',
+            detalhe: error.message
+        });
+    }
+});
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor Express rodando na porta ${PORT}`);
 });
