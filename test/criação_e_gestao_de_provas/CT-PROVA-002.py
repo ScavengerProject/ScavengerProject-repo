5aec55301803b7272e9ebb08573e57d42dd06202
Pietra Soares <pietra.soares2013@hotmail.com>
2025-11-19 21:39:06 -0300

    test: CT-RANK-003 concluído com falha.

--- test/criação_e_gestao_de_provas/CT-PROVA-002.py
+++ test/criação_e_gestao_de_provas/CT-PROVA-002.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 import time
 import os
 
